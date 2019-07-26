ea7a70af70fe16bf8cdc81a007c7844d60ab1035
Mangoman544 <mangoman544@gmail.com>
2019-07-26 16:38:51 -0700

    07/26/2019 at 4:38 pm

---
+++ src/mannyfinaljava/MannyFinalJava.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mannyfinaljava;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class MannyFinalJava {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Manny's Address Book! ");
+        System.out.println("                               ");
+        System.out.println("Number of Contacts Total: 4 ");
+        
+        System.out.println("                              ");
+        int age = 20;
+        double height = 5.9;
+        String firstName = "James";
+        String lastName = "Charles";
+        boolean makeup = true;
+        String email = "jamescharles69@gmail.com";
+        int phoneNumber=2137288522;
+        
+        System.out.println("My name is " + firstName + " " + lastName + ". " +
+                "I am " + age + " and I'm " + height + " feet tall. ");
+        System.out.println("I am a makeup Youtuber:" + makeup);
+        System.out.println("My email is: " + email); 
+        System.out.println("My phone number is: " + phoneNumber);
+        System.out.println("                                  ");
+        
+        age = 45;
+        height = 6.2;
+        firstName = "Mark";
+        lastName = "Cuben";
+        boolean male = true;
+        
+        email = "Markcuben@gmail.com";
+        phoneNumber=378499021;
+        System.out.println("My name is " + firstName + " " + lastName + ". " +
+                "I am " + age + " and I'm " + height + " feet tall. ");
+        System.out.println("I am a male: " + male);
+        System.out.println("My email is: " + email); 
+        System.out.println("My phone number is: " + phoneNumber);
+        System.out.println("                                  ");
+    
+        age = 16;
+        height = 6.3;
+        firstName = "Johnny";
+        lastName = "Brozovich";
+        male = true;
+        email = "brozovichjohnny21@serrahs.com";
+        phoneNumber=650572927;
+        
+        System.out.println("My name is " + firstName + " " + lastName + ". " +
+                "I am " + age + " and I'm " + height + " feet tall. ");
+        System.out.println("I am a male: " + male);
+        System.out.println("My email is: " + email); 
+        System.out.println("My phone number is: " + phoneNumber);
+        System.out.println("                                  ");
+        
+        age = 16;
+        height = 5.11;
+        firstName = "Weston";
+        lastName = "Warren";
+        male = true;
+        email = "westonthewall@gmail.com";
+        phoneNumber=650723455;
+        
+        System.out.println("My name is " + firstName + " " + lastName + ". " +
+                "I am " + age + " and I'm " + height + " feet tall. ");
+        System.out.println("I am a male: " + male);
+        System.out.println("My email is: " + email); 
+        System.out.println("My phone number is: " + phoneNumber);
+        System.out.println("                                  ");
+    
+        System.out.println("Thanks for Listening to me ");
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+    
+    /**
+     * @param args the command line arguments
+     */
+        // TODO code application logic here
+
+    
+
